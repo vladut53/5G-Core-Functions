d147111f00177ef81416c8db8168abd5e9e34066
Vladutz <128462671+vladut53@users.noreply.github.com>
2024-03-27 13:46:50 +0200

    Add files via upload

---
+++ GetLogs.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+
+MSG="GET /healthcheck"
+namespaces=('nf-amf' 'nf-nrf' 'nf-smf' 'nf-pcrf')
+#namespaces=$(microk8s kubectl get namespaces | grep -E 'mf|nrf|pcrf' | tr -s " " | cut -d" " -f1 | tr '\n' ' ')
+OUTPUT="/home/vladut/Downloads/5G-Core-Functions-main/OUTPUTS/OUTPUTS.txt"
+rm -fr $OUTPUT
+
+
+for NAMESPACE in "${namespaces[@]}"; do
+        echo "PODS LOGS FROM THE NAMESPACE $NAMESPACE" >> $OUTPUT
+        echo -e >> $OUTPUT
+        POD=$(microk8s kubectl get pods -n $NAMESPACE | tr -s " " | cut -d" " -f1 | tail -n1)
+        microk8s kubectl logs $POD -n $NAMESPACE | grep -v "$MSG" >> $OUTPUT
+        echo -e "===========================================================================================================================================================" >> $OUTPUT
+        echo -e >> $OUTPUT
+        echo -e >> $OUTPUT
+        echo -e >> $OUTPUT
+done
+
