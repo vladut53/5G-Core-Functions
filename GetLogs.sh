c3591618a3c09f6acdf5a969a97c05baa9a161b8
Vladutz <128462671+vladut53@users.noreply.github.com>
2024-04-08 11:21:35 +0300

    Update GetLogs.sh

--- GetLogs.sh
+++ GetLogs.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
 
+
+
+
 MSG="GET /healthcheck"
-#namespaces=('nf-amf' 'nf-nrf' 'nf-smf' 'nf-pcrf')
+
 namespaces=($(microk8s kubectl get namespaces | grep -E 'mf|nrf|pcrf' | tr -s " " | cut -d" " -f1))
+
 tmp=$(pwd)
+
 OUTPUT="$tmp/logs.txt"
+
 rm -fr $OUTPUT
 
 
 
+
+
 for NAMESPACE in "${namespaces[@]}"; do
+
         echo "PODS LOGS FROM THE NAMESPACE $NAMESPACE" >> $OUTPUT
+
         echo -e >> $OUTPUT
+
         POD=$(microk8s kubectl get pods -n $NAMESPACE | tr -s " " | cut -d" " -f1 | tail -n1)
+
         microk8s kubectl logs $POD -n $NAMESPACE | grep -v "$MSG" >> $OUTPUT
+
         echo -e "===========================================================================================================================================================" >> $OUTPUT
+
         echo -e >> $OUTPUT
+
         echo -e >> $OUTPUT
+
         echo -e >> $OUTPUT
+
 done
 
